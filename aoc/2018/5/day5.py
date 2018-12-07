1f31326e04e41498acb79151b756d1258f8577ec
Chris <cwwalker85@gmail.com>
2018-12-07 08:07:05 -0600

    Switching spaces to tabs

--- aoc/2018/5/day5.py
+++ aoc/2018/5/day5.py
@@ -36,36 +36,36 @@ part2 = 'aAbbbcCCcdDDD'
 
 for i in alphabet:
 
-    lower = []
-    upper = []
-    
-    # part2 = open('inputs.txt').read()
-    if (i=='a'): part2 = 'aAbbbcCCcdDDD'
-    found = True
+	lower = []
+	upper = []
+	
+	# part2 = open('inputs.txt').read()
+	if (i=='a'): part2 = 'aAbbbcCCcdDDD'
+	found = True
 
-    while (found == True):
-        found = False
-    	for j in alphabet:
-    		if(i != j):
-        		checkChar = i + checkCase(i)
-        		startPosition = part2.find(checkChar)
-        
-        		if (startPosition > -1):            
-            		part2 = part2[:startPosition] + part2[startPosition+2:]
-            		found = True
+	while (found == True):
+		found = False
+		for j in alphabet:
+			if(i != j):
+				checkChar = i + checkCase(i)
+				startPosition = part2.find(checkChar)
 
-    if (i.islower() == True):
-        lower.append(len(part2))
-    else:
-        upper.append(len(part2))
-        
-    print(i)
-    print(part2)
-    
+				if (startPosition > -1):            
+					part2 = part2[:startPosition] + part2[startPosition+2:]
+					found = True
+
+	if (i.islower() == True):
+		lower.append(len(part2))
+	else:
+		upper.append(len(part2))
+
+	print(i)
+	print(part2)
+
 # union = zip(lower,upper)
 
 # for pair in union:
-    # results.append(pair[0] + pair[1])
+# results.append(pair[0] + pair[1])
 
 # print(results)
 # print(min(results))
