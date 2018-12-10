080daebe6a8006b09d7742a829f04cb895176ae4
Chris <cwwalker85@gmail.com>
2018-12-10 08:58:57 -0600

    Working part 1

--- aoc/2018/3/day3.py
+++ aoc/2018/3/day3.py
@@ -3,9 +3,9 @@ import itertools
 import sys
 import string
 
-# sample = """#1 @ 1,3: 4x4
-# #2 @ 3,1: 4x4
-# #3 @ 5,5: 2x2"""
+#sample = """#1 @ 1,3: 4x4
+##2 @ 3,1: 4x4
+##3 @ 5,5: 2x2"""
 
 data = []
 
@@ -13,13 +13,21 @@ with open('inputs.txt') as f:
     for line in f:
         data.append(line)
 
-# sampleArr = sample.split('#')
-# for line in sampleArr:
-#     data.append(line)
+#sampleArr = sample.split('#')
+#for line in sampleArr:
+#    data.append(line)
 
-# data.pop(0)
+#data.pop(0)
 
 allData = []
+
+# building matrix for plot overlaps
+if (len(allData) == 0):
+    for a in range(1000): # Vertical Rows
+        col = []
+        for b in range(1000): # Horizontal Columns
+            col.append(0)
+        allData.append(col)
 
 for i in data:
     areaArr = i.split(' ')
@@ -34,26 +42,11 @@ for i in data:
     dimX = int(dimensions[0])
     dimY = int(dimensions[1].replace('\n',''))
 
-    # ending coorindate
-    endingX = startingX + dimX
-    endingY = startingY + dimY
-
-    print(areaArr)
-    print(str(startingX) + ',' + str(startingY) + ' through ' + str(endingX) + ',' + str(endingY))
-    
-    # building matrix for plot overlaps
-    if (len(allData) == 0):
-        for a in range(1000): # Vertical Rows
-            col = []
-            for b in range(1000): # Horizontal Columns
-                col.append(0)
-            allData.append(col)
-
-    for c in range(len(allData)):
-        if (startingY >= c and c <= endingY):
+    for y in range(startingY,startingY+dimY):
+        # if (startingY >= c and c <= endingY):
             
-            for d in range(len(allData[c])):
-                if (startingX >= d and d <= endingX):
-                    allData[c][d] += 1
+            for x in range(startingX,startingX+dimX):
+                # if (startingX >= d and d <= endingX):
+                    allData[y][x] += 1
 
 print(allData)
