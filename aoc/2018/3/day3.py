645543c8864b2650e7d46e012c062094727f9b07
Chris <cwwalker85@gmail.com>
2018-12-09 22:52:49 -0600

    adding day3 part 1

--- aoc/2018/3/day3.py
+++ aoc/2018/3/day3.py
@@ -3,26 +3,57 @@ import itertools
 import sys
 import string
 
-alphabet = string.ascii_lowercase + string.ascii_uppercase
-string = open('inputs.txt').read()
+# sample = """#1 @ 1,3: 4x4
+# #2 @ 3,1: 4x4
+# #3 @ 5,5: 2x2"""
 
-def checkCase(c):
-    val = ''
-    if (c.isupper()): val = c.lower()
-    else: val = c.upper()
-    return val
+data = []
 
-found = True
-while (found == True):
-    found = False
+with open('inputs.txt') as f:
+    for line in f:
+        data.append(line)
 
-    for i in alphabet:
-        checkChar = i + checkCase(i)
-        startPosition = string.find(checkChar)
-        
-        if (startPosition > -1):            
-            string = string[:startPosition] + string[startPosition+2:]
-            found = True
+# sampleArr = sample.split('#')
+# for line in sampleArr:
+#     data.append(line)
 
+# data.pop(0)
 
-print(len(string))
+allData = []
+
+for i in data:
+    areaArr = i.split(' ')
+    areaLen = len(areaArr)
+
+    # starting coordinate
+    starting = areaArr[2].split(',')
+    startingX = int(starting[0])
+    startingY = int(starting[1].replace(':',''))
+
+    dimensions = areaArr[3].split('x')
+    dimX = int(dimensions[0])
+    dimY = int(dimensions[1].replace('\n',''))
+
+    # ending coorindate
+    endingX = startingX + dimX
+    endingY = startingY + dimY
+
+    print(areaArr)
+    print(str(startingX) + ',' + str(startingY) + ' through ' + str(endingX) + ',' + str(endingY))
+    
+    # building matrix for plot overlaps
+    if (len(allData) == 0):
+        for a in range(1000): # Vertical Rows
+            col = []
+            for b in range(1000): # Horizontal Columns
+                col.append(0)
+            allData.append(col)
+
+    for c in range(len(allData)):
+        if (startingY >= c and c <= endingY):
+            
+            for d in range(len(allData[c])):
+                if (startingX >= d and d <= endingX):
+                    allData[c][d] += 1
+
+print(allData)
